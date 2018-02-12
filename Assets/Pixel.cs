aba7af5bad20d6470ec04679610e82094b8d117d
Raymond Perminov <raymondproduction@ukr.net>
2018-02-12 21:53:52 +0200

    first

---
+++ Assets/Pixel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pixel : MonoBehaviour {
+	public float speed =3f;
+	public GameObject shot_original, obj;
+	private GameObject shot;
+
+	void Start(){
+		//shot = Instantiate(shot_original, transform.position, Quaternion.identity) as GameObject;
+	}
+	void Update(){
+		
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Translate(Vector2.left * speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Translate(Vector2.right * speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.Escape)) {
+			Application.Quit();
+		}
+		if (Input.GetKey(KeyCode.Space)) {
+			StartCoroutine(instObj());
+			//shot = Instantiate(shot_original, transform.position, Quaternion.identity) as GameObject;
+		}
+	}
+
+     IEnumerator instObj () {
+		 yield return new WaitForSeconds(1.5f);
+		 Instantiate(shot_original, transform.position, Quaternion.identity);
+	 }
+}
